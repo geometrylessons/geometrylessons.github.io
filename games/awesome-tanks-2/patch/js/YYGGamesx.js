e2031cbf64fd0b745dc31b83704f9f33cb9c3279
class-80x <cansuakcay12@gmail.com>
2024-12-19 21:39:45 +0300

    w

--- games/awesome-tanks-2/patch/js/YYGGamesx.js
+++
@@ -1,9 +0,0 @@
-function YYGG4M3S () {
-  this.showSplash = function (data= null) {
-    console.log("--fx--YYGG4M3S--showSplash--");
-  }
-  this.startupByYad= function (obj) {
-    console.log("--fx--YYGG4M3S--startupByYad--", obj);
-  }
-}
-YYGG4M3S= new YYGG4M3S();
